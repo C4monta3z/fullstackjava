14a9f9fa822ac919fc4217d8418e6ba24d1304a1
fabian <c4monta3z@gmail.com>
2020-09-28 17:37:25 -0500

    first commit fullstack java

---
+++ index.js
@@ -0,0 +1,49 @@
+/* console.log ("hola mundo");
+  */
+ let lista=[2,4,1,3,6,8,9,7,5];
+ function obterMayor(lista){
+     return Math.max.apply(null, lista)
+ }
+//console.log(lista.filter(item => item >obterMayor(lista)/2));
+/* cr=Math.max.apply(null, lista)/2 
+console.log(lista.filter(item => item >cr));
+ */
+let n = 4;
+var sumZero = function(n) {
+
+     Math.random
+     
+ };
+
+/*  let olista=[];
+ lista.forEach(item => f);{
+     if (item > lista.length){
+        console.log(item);
+     }
+ } */
+ /* let res;
+ for(let i=0;lista.filter.length; i++){
+    console.log("Resultado"+res);
+ }
+  */
+
+//###############   others web ##########
+/*let msg = 'hello world'
+$('#header')
+  .html(msg)
+  .fadeOut(1000)
+  console.log({ myMessage: msg })*/
+
+  /* let miLista = [3,5,8,6,2,12,1,5,3];
+  //miLista = miLista.sort((a,b)=> (a,b)) 
+  
+  //console.log("pares")
+  let milistaResultante=[];
+   for(i=0; i<miLista.length; i++){
+     if(miLista[i]>=5){
+       milistaResultante.push(miLista[i]);
+     }
+   }
+  console.log("Resul",milistaResultante);
+  
+   */
