51acbbd3a33cad5209dd2caf04a4aeffadf8fd57
C4monta3z <48732522+C4monta3z@users.noreply.github.com>
2020-10-28 20:02:13 -0500

    solucionar logica
    react

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button, Row } from 'reactstrap';
+
+// Components:
+import ProductTable from './components/ProductTable';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentOperator: 5
+    }
+  }
+
+  render() {
+    return (
+      <Row style={{
+        justifyContent: 'center',
+        marginTop: 50,
+      }}>
+        <Button onClick={() => {
+          this.setState({ currentOperator: this.state.currentOperator - 1 })
+        }}>{'<'}</Button>
+
+        <ProductTable number={this.state.currentOperator} />
+
+        <Button onClick={() => {
+          this.setState({ currentOperator: this.state.currentOperator + 1 })
+        }}>{'>'}</Button>
+      </Row>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,49 +1,19 @@
-/* console.log ("hola mundo");
-  */
- let lista=[2,4,1,3,6,8,9,7,5];
- function obterMayor(lista){
-     return Math.max.apply(null, lista)
- }
-//console.log(lista.filter(item => item >obterMayor(lista)/2));
-/* cr=Math.max.apply(null, lista)/2 
-console.log(lista.filter(item => item >cr));
- */
-let n = 4;
-var sumZero = function(n) {
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-     Math.random
-     
- };
+// Main component:
+import App from './App';
 
-/*  let olista=[];
- lista.forEach(item => f);{
-     if (item > lista.length){
-        console.log(item);
-     }
- } */
- /* let res;
- for(let i=0;lista.filter.length; i++){
-    console.log("Resultado"+res);
- }
-  */
+// Worker:
+import reportWebVitals from './reportWebVitals';
 
-//###############   others web ##########
-/*let msg = 'hello world'
-$('#header')
-  .html(msg)
-  .fadeOut(1000)
-  console.log({ myMessage: msg })*/
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  /* let miLista = [3,5,8,6,2,12,1,5,3];
-  //miLista = miLista.sort((a,b)=> (a,b)) 
-  
-  //console.log("pares")
-  let milistaResultante=[];
-   for(i=0; i<miLista.length; i++){
-     if(miLista[i]>=5){
-       milistaResultante.push(miLista[i]);
-     }
-   }
-  console.log("Resul",milistaResultante);
-  
-   */
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
